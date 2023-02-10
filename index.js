64deb5857787b10e97e934e2b49ba5cfce58aeba
NguyenMinhTuan <tuannb0mmm.2x1y9z@gmail.com>
2023-02-10 08:47:56 +0700

    Initial comit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//Route
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
+
+// 127.0.0.1 - localhost
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+})
